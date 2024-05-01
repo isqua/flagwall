770a143b0dd3a61c00463e81f4b1dbce8890b656
isqua <isqua@isqua.ru>
2024-05-02 00:09:30 +0900

    chore: add flags icons to dist

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+export default defineConfig({
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                { 
+                    src:  'node_modules/flag-icons/flags/4x3/*.svg',
+                    dest: 'node_modules/flag-icons/flags/4x3/',
+                }
+            ],
+        }),
+    ],
+});
