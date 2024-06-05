d46e407212ab13ee7c6110997305f3010da47b80
isqua <isqua@isqua.ru>
2024-06-06 02:32:26 +0700

    fix: do not show the Arab league because it is not a country

--- src/flags/countries.ts
+++ src/flags/countries.ts
@@ -2,6 +2,6 @@ import flags from 'flag-icons/country.json';
 
 import type { Country } from 'flag-icons';
 
-const SKIP = new Set(["eu", "xx", "un", "cefta"]);
+const SKIP = new Set(["eu", "xx", "un", "cefta", "arab"]);
 
 export const countries: Country[] = flags.filter(flag => !SKIP.has(flag.code));
