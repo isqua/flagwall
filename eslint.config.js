c1378e6b65e65f951149966067f5a90a2bfc4c0a
isqua <isqua@isqua.ru>
2024-06-13 14:31:55 +0700

    chore: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strict,
+    {
+        ignores: ['dist'],
+    },
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            }
+        }
+    },
+    {
+        files: ['*.js'],
+        extends: [tseslint.configs.disableTypeChecked],
+    },
+);
