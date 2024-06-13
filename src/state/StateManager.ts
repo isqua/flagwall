a6398c6216c7601900a55be2925c39ef54c78905
isqua <isqua@isqua.ru>
2024-06-13 15:16:21 +0700

    feat: save countries to the localstorage

--- src/state/StateManager.ts
+++ src/state/StateManager.ts
@@ -1,9 +1,23 @@
+const COUNTRIES_STORAGE_NAME = "countries";
+
 export class StateManager {
     getCodes(): string[] {
-        return window.location.hash.slice(1).split(",");
+        let countriesList = window.location.hash.slice(1);
+
+        if (countriesList.length === 0 && localStorage) {
+            countriesList = localStorage.getItem(COUNTRIES_STORAGE_NAME) ?? "";
+        }
+
+        return countriesList.split(",");
     }
 
     writeCodes(codes: string[]) {
-        window.location.hash = codes.join(",");
+        const countriesList = codes.join(",");
+
+        window.location.hash = countriesList;
+
+        if (localStorage) {
+            localStorage.setItem(COUNTRIES_STORAGE_NAME, countriesList);
+        }
     }
 }
