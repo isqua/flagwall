f44c2e21cebfcd72512f417d0209b3d32c01533b
isqua <isqua@isqua.ru>
2024-08-23 13:40:06 +0300

    fix: exclude communities

--- src/flags/countries.ts
+++ src/flags/countries.ts
@@ -2,7 +2,7 @@ import flags from "flag-icons/country.json";
 
 import type { Country } from "flag-icons";
 
-const SKIP = new Set(["eu", "xx", "un", "cefta", "arab"]);
+const SKIP = new Set(["arab", "cefta", "eac", "eu", "pc", "un", "xx"]);
 
 export const countries: Country[] = flags.filter(
     (flag) => !SKIP.has(flag.code),
